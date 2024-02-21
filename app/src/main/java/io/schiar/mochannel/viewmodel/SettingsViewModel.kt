4c06bae763f1f38a6393692ad4e2ef080e785bfe
Giovani Schiar <giovanischiar@gmail.com>
2024-02-20 20:05:11 -0800

    Remove Log on ViewModel

--- app/src/main/java/io/schiar/mochannel/viewmodel/SettingsViewModel.kt
+++ app/src/main/java/io/schiar/mochannel/viewmodel/SettingsViewModel.kt
@@ -1,24 +1,20 @@
 package io.schiar.mochannel.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import io.schiar.mochannel.model.repository.SettingsRepository
 import io.schiar.mochannel.viewmodel.util.toViewData
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
 class SettingsViewModel(private val settingsRepository: SettingsRepository): ViewModel() {
     val serverURL = settingsRepository.serverURL.map { it.toViewData() }
-        .onEach { Log.d("mo channel debug", "viewmodel.SettingsViewModel serverURL: $it") }
 
     fun updatePrefixTo(newPrefix: String) = viewModelScope.launch {
         settingsRepository.updatePrefixTo(newPrefix = newPrefix)
     }
 
     fun updateURLTo(newURL: String) = viewModelScope.launch {
-        Log.d("mo channel debug", "settingsRepository.updateURLTo(newURL = $newURL)")
         settingsRepository.updateURLTo(newURL = newURL)
     }
 
