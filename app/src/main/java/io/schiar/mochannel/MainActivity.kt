8e1d8039142aade02cd006c81c8c8c5584c90a9b
Giovani Schiar <giovanischiar@gmail.com>
2024-02-27 15:24:01 -0800

    Remove AppScreen, Replace it With the Private Composable Method Called Navigation Inside Activity

--- app/src/main/java/io/schiar/mochannel/MainActivity.kt
+++ app/src/main/java/io/schiar/mochannel/MainActivity.kt
@@ -1,11 +1,23 @@
 package io.schiar.mochannel
 
 import android.os.Bundle
+import android.util.Patterns
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import io.schiar.mochannel.library.local.PreviewLocalData
 import io.schiar.mochannel.model.datasource.ServerURLDataSource
@@ -18,11 +30,17 @@ import io.schiar.mochannel.model.repository.SettingsRepository
 import io.schiar.mochannel.model.repository.TVShowRepository
 import io.schiar.mochannel.model.repository.TVShowsRepository
 import io.schiar.mochannel.model.repository.VideoRepository
-import io.schiar.mochannel.view.screen.AppScreen
+import io.schiar.mochannel.view.screen.SettingsScreen
+import io.schiar.mochannel.view.screen.TVShowScreen
+import io.schiar.mochannel.view.screen.TVShowsScreen
+import io.schiar.mochannel.view.screen.VideoScreen
+import io.schiar.mochannel.view.viewdata.ServerURLViewData
 import io.schiar.mochannel.viewmodel.SettingsViewModel
 import io.schiar.mochannel.viewmodel.TVShowViewModel
 import io.schiar.mochannel.viewmodel.TVShowsViewModel
 import io.schiar.mochannel.viewmodel.VideoViewModel
+import kotlinx.coroutines.flow.drop
+import kotlinx.coroutines.flow.first
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -33,13 +51,51 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            AppScreen(
-                settingsViewModel = settingsViewModel,
-                tvShowsViewModel = tvShowsViewModel,
-                tvShowViewModel = tvShowViewModel,
-                videoViewModel = videoViewModel
-            )
+        setContent { Navigation() }
+    }
+
+    @Composable
+    private fun Navigation(
+        settingsViewModel: SettingsViewModel = this.settingsViewModel,
+        tvShowsViewModel: TVShowsViewModel = this.tvShowsViewModel,
+        tvShowViewModel: TVShowViewModel = this.tvShowViewModel,
+        videoViewModel: VideoViewModel = this.videoViewModel,
+    ) {
+        val navController = rememberNavController()
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val serverURL by settingsViewModel.serverURL.collectAsState(initial = ServerURLViewData())
+        LaunchedEffect(serverURL) {
+            val (fullURL) = settingsViewModel.serverURL.drop(count = 1).first()
+            val isServerURLValid = Patterns.WEB_URL.matcher(fullURL).matches()
+            if (!isServerURLValid && navBackStackEntry?.destination?.route != "Settings") {
+                navController.navigate("Settings")
+            }
+        }
+
+        Box {
+            NavHost(
+                modifier = Modifier.background(color = colorResource(R.color.backgroundColor)),
+                navController = navController,
+                startDestination = "TVShows"
+            ) {
+                composable(route = "Settings") {
+                    SettingsScreen(settingsViewModel = settingsViewModel)
+                }
+                composable(route = "TVShows") {
+                    TVShowsScreen(
+                        tvShowsViewModel = tvShowsViewModel,
+                        onSettingsPressed = { navController.navigate(route = "Settings") },
+                        onTVShowPressed = { navController.navigate(route = "TVShow") }
+                    )
+                }
+                composable(route = "TVShow") {
+                    TVShowScreen(
+                        tvShowViewModel = tvShowViewModel,
+                        onVideoPressed = { navController.navigate(route = "Video") }
+                    )
+                }
+                composable(route = "Video") { VideoScreen(videoViewModel = videoViewModel) }
+            }
         }
     }
 
@@ -54,7 +110,7 @@ class MainActivity : ComponentActivity() {
         ) = createRepositories(
             serverURLDataSource = serverURLLocalService, tvShowsDataSource = tvShowsLocalService
         )
-        AppScreen(
+        Navigation(
             settingsViewModel = SettingsViewModel(settingsRepository = settingsRepository),
             tvShowsViewModel = TVShowsViewModel(tvShowsRepository = tvShowsRepository),
             tvShowViewModel = TVShowViewModel(tvShowRepository = tvShowRepository),
@@ -70,7 +126,7 @@ class MainActivity : ComponentActivity() {
         ) = createRepositories(
             serverURLDataSource = ServerURLLocalDataSource(), tvShowsDataSource = TVShowsLocalDataSource()
         )
-        AppScreen(
+        Navigation(
             settingsViewModel = SettingsViewModel(settingsRepository = settingsRepository),
             tvShowsViewModel = TVShowsViewModel(tvShowsRepository = tvShowsRepository),
             tvShowViewModel = TVShowViewModel(tvShowRepository = tvShowRepository),
--- app/src/main/java/io/schiar/mochannel/view/screen/TVShowsScreen.kt
+++ app/src/main/java/io/schiar/mochannel/view/screen/TVShowsScreen.kt
@@ -6,18 +6,29 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.tv.material3.ExperimentalTvMaterial3Api
+import androidx.tv.material3.Icon
+import io.schiar.mochannel.R
 import io.schiar.mochannel.view.components.ListView
 import io.schiar.mochannel.viewmodel.TVShowsViewModel
 
+@OptIn(ExperimentalTvMaterial3Api::class)
 @Composable
-fun TVShowsScreen(tvShowsViewModel: TVShowsViewModel, onTVShowPressed: () -> Unit = {}) {
+fun TVShowsScreen(
+    tvShowsViewModel: TVShowsViewModel,
+    onSettingsPressed: () -> Unit = {},
+    onTVShowPressed: () -> Unit = {}
+) {
     val tvShows by tvShowsViewModel.tvShows.collectAsState(initial = emptyList())
     val loading by tvShowsViewModel.loading.collectAsState()
     val errorResponse by tvShowsViewModel.errorMessage.collectAsState()
@@ -41,6 +52,19 @@ fun TVShowsScreen(tvShowsViewModel: TVShowsViewModel, onTVShowPressed: () -> Uni
             Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
                 CircularProgressIndicator()
             }
+        }
+
+        IconButton(
+            modifier = Modifier
+                .align(alignment = Alignment.TopEnd)
+                .padding(top = 50.dp, end = 50.dp),
+            onClick = onSettingsPressed
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_settings_24),
+                contentDescription = "Go to settings screen",
+                tint = colorResource(id = R.color.moonColor)
+            )
         }
     }
 }
